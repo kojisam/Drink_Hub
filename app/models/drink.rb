62db97da3c2b9eff1be8455a72d7bed2aaa8eccb
Koji Samuels <kojisamuels@gmail.com>
2022-11-01 19:47:48 +0000

    updated form -working

--- app/models/drink.rb
+++ app/models/drink.rb
@@ -4,6 +4,6 @@ class Drink < ApplicationRecord
   validates :name, presence: true
   validates :price, presence: true
   validates :description, presence: true
-  validates :image, presence: true
+  # validates :image, presence: true
   validates :stock_level, numericality: { greater_than: 0 }
 end
