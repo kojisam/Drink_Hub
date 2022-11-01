24800dfdd1133261d5673dd98ce5b7702e174561
Abdul Elmi <abs.sw6@gmail.com>
2022-11-01 19:22:55 +0000

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,16 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-
+puts "Destroy all records"
 Order.destroy_all
 Drink.destroy_all
 User.destroy_all
-
+puts "Done"
+puts "Creating test user"
 User.create!(
   email: "fakertest@email.com",
   password: "faker123345"
 )
-
+puts "Done"
+puts "Creating 50 drinks"
 50.times do
   Drink.create!(
     name: Faker::Kpop.iii_groups,
@@ -24,7 +26,8 @@ User.create!(
     user: User.first
   )
 end
-
+puts "Done"
+puts "Creating 10 orders"
 10.times do
   Order.create!(
     quantity: rand(1..10),
@@ -32,3 +35,4 @@ end
     user: User.first
   )
 end
+puts "Done"
