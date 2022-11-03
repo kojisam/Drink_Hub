5f39f025b8dc48bb28ccc5fcdf02ddd7098a6ed8
Koji Samuels <kojisamuels@gmail.com>
2022-11-03 20:43:07 +0000

    working links with faker images and cloudinary

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,9 @@ Rails.application.routes.draw do
 
   root to: "pages#home"
 
-  # resources :drinks
-  get "/home", to: "drinks#index"
-  get "users/:id", to: "users#show"
-  get "drinks/new", to: "drinks#new"
-  post "drinks", to: "drinks#create"
-  get "orders", to: "orders#index"
-  post "drinks/:id", to: "drinks#show"
-  get "orders/:id/show", to: "orders#show"
-  get "dashboard", to: "users#dashboard"
+  resources :drinks do
+    resources :orders, only: [:show, :create]
+  end
+
+  resources :orders, only: :index
 end
