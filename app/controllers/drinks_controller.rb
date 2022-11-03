2940a54655e766b35bbb48f6aef307bb87b3a340
Koji Samuels <kojisamuels@gmail.com>
2022-11-03 21:05:40 +0000

    tested linking of new form

--- app/controllers/drinks_controller.rb
+++ app/controllers/drinks_controller.rb
@@ -11,8 +11,13 @@ class DrinksController < ApplicationController
   # will come back to create later
   def create
     @drink = Drink.new(drink_params)
-    @drink.save
-    redirect_to drinks_path(@drink)
+    @drink.user = current_user
+    if @drink.save!
+      redirect_to drinks_path(@drink)
+    else
+      @drink = Drink.new
+      render 'drinks/show', status: :unprocessable_entity
+    end
   end
 
   def show
