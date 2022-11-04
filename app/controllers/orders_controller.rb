924a20fafabcfcf15d03a28dec4282f950d8cebe
Gianluca Galli <gianluca.galli9519@gmail.com>
2022-11-04 19:25:58 +0000

    controller order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,4 +6,20 @@ class OrdersController < ApplicationController
   def show
     @order = Order.find(params[:id])
   end
+
+  def new
+    @order = Order.new
+  end
+
+  def create
+    @order = Order.new(order_params)
+    @order.save
+    redirect_to orders_path(@order)
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:quantity, :drink_id)
+  end
 end
