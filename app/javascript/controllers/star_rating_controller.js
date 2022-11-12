7d2fdb4ad764243a6495f57f609ff6aa25235cdd
Koji Samuels <kojisamuels@gmail.com>
2022-11-12 14:44:38 +0000

    null

--- app/javascript/controllers/star_rating_controller.js
+++ app/javascript/controllers/star_rating_controller.js
@@ -4,7 +4,6 @@ import StarRating from "star-rating.js"
 // Connects to data-controller="star-rating"
 export default class extends Controller {
   connect() {
-    console.log("hello world")
     new StarRating(this.element)
   }
 }
